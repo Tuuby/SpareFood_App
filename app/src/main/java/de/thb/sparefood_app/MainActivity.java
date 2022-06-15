c0071f026281b717dcd728f391d3d648c2560028
suriver11 <suriver11@hotmail.de>
2022-06-15 20:06:24 +0200

    fab navigates to newEntryFragment

--- app/src/main/java/de/thb/sparefood_app/MainActivity.java
+++ app/src/main/java/de/thb/sparefood_app/MainActivity.java
@@ -8,12 +8,15 @@ import android.view.WindowManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import de.thb.sparefood_app.databinding.ActivityMainBinding;
+import de.thb.sparefood_app.ui.home.HomeFragment;
 import de.thb.sparefood_app.ui.new_entry.NewEntryFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,13 +42,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createNewEntry(View view) {
-        getSupportFragmentManager().beginTransaction().add(R.id.nav_host_fragment_activity_main, new NewEntryFragment()).commit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setReorderingAllowed(true);
+        NewEntryFragment newEntryFragment = new NewEntryFragment();
+
+
+//        transaction.replace(R.id.nav_host_fragment_activity_main, NewEntryFragment.class, null).commit();
 //        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navController.navigate(R.id.navigation_new_entry);
         Log.d("TestTag", "createNewEntryLOGGER");
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 
     public void closeNewEntry(View view) {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navController.navigate(R.id.navigation_home);
 //        getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }

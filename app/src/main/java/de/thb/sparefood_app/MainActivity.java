b36bece6564378c08cff8afddb3703a3831525a0
suriver11 <suriver11@hotmail.com>
2022-06-28 17:00:57 +0200

    temp commit unfinished cameraTest

--- app/src/main/java/de/thb/sparefood_app/MainActivity.java
+++ app/src/main/java/de/thb/sparefood_app/MainActivity.java
@@ -1,9 +1,16 @@
 package de.thb.sparefood_app;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -26,6 +33,10 @@ import de.thb.sparefood_app.ui.new_entry.NewEntryFragment;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private static final int pic_id = 123;
+    // Define the button and imageview type variable
+    ImageButton camera_open_id;
+    ImageView click_image_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +54,48 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // CAMERA Logic
+        camera_open_id = (ImageButton)findViewById(R.id.cameraButton);
+        click_image_id = (ImageView)findViewById(R.id.mealImage);
+
+        camera_open_id.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+
+                // Create the camera_intent ACTION_IMAGE_CAPTURE
+                // it will open the camera for capture the image
+                Intent camera_intent
+                        = new Intent(MediaStore
+                        .ACTION_IMAGE_CAPTURE);
+
+                // Start the activity with camera_intent,
+                // and request pic id
+                startActivityForResult(camera_intent, pic_id);
+            }
+        });
+
+    }
+
+    // This method will help to retrieve the image
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+
+        // Match the request 'pic id with requestCode
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == pic_id) {
+
+            // BitMap is data structure of image file
+            // which stor the image in memory
+            Bitmap photo = (Bitmap) data.getExtras()
+                    .get("data");
+
+            // Set the image in imageview for display
+            click_image_id.setImageBitmap(photo);
+        }
     }
 
     public void createNewEntry(View view) {

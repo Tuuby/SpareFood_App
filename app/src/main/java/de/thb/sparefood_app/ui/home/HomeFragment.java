f7dae8708e09a910e83d1448493ef89baabf8770
Tuuby <tobiastrompell@aol.com>
2022-07-12 12:16:03 +0200

    Clean up imports

--- app/src/main/java/de/thb/sparefood_app/ui/home/HomeFragment.java
+++ app/src/main/java/de/thb/sparefood_app/ui/home/HomeFragment.java
@@ -2,17 +2,11 @@ package de.thb.sparefood_app.ui.home;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import androidx.annotation.GravityInt;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
